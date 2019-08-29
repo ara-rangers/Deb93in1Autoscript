4a71a86dceb45d13c11a697521865dc47410881d
jm051484 <31314849+jm051484@users.noreply.github.com>
2019-08-29 13:53:34 +0800

    Update VPSnOCScrptZ.sh

--- VPSnOCScrptZ.sh
+++ VPSnOCScrptZ.sh
@@ -18,7 +18,22 @@ else
 	source="https://raw.githubusercontent.com/jm051484/Deb93in1Autoscript/master"
 fi
 
-# initialisasi var
+clear
+echo ""
+echo "I need to ask some questions before starting setup"
+echo "You can leave the default option and just hit enter if you agree with the option"
+echo ""
+echo "First I need to know the new password of MySQL root user:"
+read -p "Password baru: " -e -i jm051484 DatabasePass
+echo ""
+echo "Finally, name the Database Name for OCS Panels"
+echo " Please, use one word only, no special characters other than Underscore (_)"
+read -p " Database Name: " -e -i OCS_PANEL DatabaseName
+echo ""
+echo "Okay, that's all I need. We are ready to setup your OCS Panels now"
+read -n1 -r -p "Press any key to continue..."
+
+# initializing var
 export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
 MYIP=$(wget -qO- ipv4.icanhazip.com);
